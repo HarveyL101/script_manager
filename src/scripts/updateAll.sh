d424f3d9a211654c012081d950ab4a0f1db2b2c9
Harvey Lopez <up2118183@myport.ac.uk>
2026-07-07 14:58:42 +0100

    Improved upon pkgman detection, scans for common packages and opts for user input if unsuccessful.

--- src/scripts/updateAll.sh
+++ src/scripts/updateAll.sh
@@ -15,13 +15,36 @@ updateAll() {
 			"putty"	
 		       )
 	
+	// Currently checks for apt, dnf and yum
 	package_manager="USER INPUT"
-	read -p "Enter your package manager command (E.g apt, dnf, ...)" "$package_manager"
+	if [ command -v apt >/dev/null 2>&1 ]; then
+		package_manager="apt"
+	else if [ command -v dnf >/dev/null 2>&1]; then
+		package_manager="dnf"
+	else if [ command -v yum >/dev/null 2>&1]; then
+		package_manager="yum"
+	else 
+		package_manager="missing"
+	fi
 
-	echo "Updating dev softwares..."
-	
+	if [ "$package_manager"=="missing" ]; then
+		res=''
+		read -p "Package manager not found, would you like to enter the name of your package manager? [y/n] (E.g. apt, dnf, yum)" "$res"
+		case "$res" in
+			[yY][eE][sS]|[yY])
+				read -p "Enter the name of your package manager in lowercase" "$package_manager"
+				;;
+			*)
+				echo "package manager declined, exiting the program now."
+				exit 1
+				;;
+		esac
+	else
+		echo "Updating dev softwares..."
+	fi
+
 	for pkg in "${arr[@]}"; do
-		if [ command -v "$pkg" >/dev/null 2>&1; ]
+		if [ command -v "$pkg" >/dev/null 2>&1 ]; then
 			echo "${arr[$i]} found, updating to the latest version now..."
 			sudo "$package_manager" update --only-upgrade "$pkg"
 		else
